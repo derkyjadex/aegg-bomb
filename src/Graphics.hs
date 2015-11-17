1ea145947c82e8230889142d2e8bfecec382681f
James Deery <james.deery@jadex.org>
2015-11-17 23:18:29 +0000

    Render scene as JSON

--- src/Graphics.hs
+++ src/Graphics.hs
@@ -1,11 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Graphics
        (RenderChan(..), sendScene, Scene(..), renderMain, newRenderChan)
        where
 
 import           Control.Concurrent
 import           Control.Monad
+import           Data.Aeson
+import qualified Data.ByteString.Lazy as B
+import           Data.HashMap.Strict
 import           Physics
-import           Text.Show.Pretty
 
 data Scene =
   Scene {_walls      :: [Box]
@@ -25,8 +28,49 @@ sendScene (RenderChan var) = swapMVar var
 readScene :: RenderChan -> IO Scene
 readScene (RenderChan var) = readMVar var
 
+wallJson :: Box -> Value
+wallJson wall =
+  Object $ fromList [ ("type", "wall")
+                    , ("bounds", toJSON wall)
+                    ]
+
+playerJson :: (String, Pos, Box) -> Value
+playerJson (name, pos, bounds) =
+  Object $ fromList [ ("type", "player")
+                    , ("name", toJSON name)
+                    , ("pos", toJSON pos)
+                    , ("bounds", toJSON bounds)
+                    ]
+
+eggJson :: (Pos, Box, Double) -> Value
+eggJson (pos, bounds, height) =
+  Object $ fromList [ ("type", "egg")
+                    , ("pos", toJSON pos)
+                    , ("bounds", toJSON bounds)
+                    , ("height", toJSON height)
+                    ]
+
+explosionJson :: (Pos, Box) -> Value
+explosionJson (pos, bounds) =
+  Object $ fromList [ ("type", "explosion")
+                    , ("pos", toJSON pos)
+                    , ("bounds", toJSON bounds)
+                    ]
+
+sceneJson :: Scene -> Value
+sceneJson (Scene walls players eggs explosions) =
+  toJSON $
+  fmap wallJson walls ++
+  fmap playerJson players ++
+  fmap eggJson eggs ++
+  fmap explosionJson explosions
+
 renderScene :: Scene -> IO ()
-renderScene scene = putStrLn (ppShow scene)
+renderScene scene = do
+  putStrLn ":begin"
+  B.putStr $ encode $ sceneJson scene
+  putStrLn ""
+  putStrLn ":end"
 
 renderMain :: RenderChan -> IO ()
 renderMain chan =
